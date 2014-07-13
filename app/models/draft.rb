7d2d24bf4d72e376476fba40f6c746d872b3208d
Cody Wilson <Cheex11@gmail.com>
2014-07-12 21:13:52 -0500

    added the function for order and saved team specs

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -6,14 +6,48 @@ class Draft < ActiveRecord::Base
 		@players = []
 		@order = []
 		@teams = []
+		@rounds = 16
 
-		print self.id
+		self.set_teams()
+		self.set_players()
 
+		self.set_order()
+
+	end
+
+	def set_order()
+		@rounds.times do |number|
+			if number.even?
+				@teams.each do |team|
+					@order << team
+				end
+			end
+
+			if number.odd?
+				@teams.reverse.each do |team|
+					@order << team
+				end
+			end
+		end
+	end
+
+	def order
+		@order
+	end
+
+	def rounds
+		@rounds
+	end
+
+
+	def set_teams()
 		Team.where(draft_id: self.id).each do |team|
 			print team.draft_id
 			@teams << team
 		end
+	end
 
+	def set_players()
 		Player.all.each do |player|
 			@players << player
 		end
--- spec/models/draft_spec.rb
+++ spec/models/draft_spec.rb
@@ -11,14 +11,6 @@ RSpec.describe Draft, :type => :model do
 			draft.start
 			draft.players.should eq [cam]
 		end
-
-		# it 'should set the order of the draft' do
-
-		# 	draft = FactoryGirl.create(:draft)
-		# 	draft.start
-
-		# end
-
 	end
 
 	describe 'number_of_teams' do
@@ -41,44 +33,7 @@ RSpec.describe Draft, :type => :model do
 
 
 
-	# describe 'initialize' do
-	# 	it 'should initialize with an array for player one' do
-	# 		draft = Draft.create(12, "snake")
-	# 		draft.teams.length.should eq 12
-	# 	end
-
-	# 	it 'should initialize with a default 16 rounds' do
-	# 		draft = Draft.create(12, "snake")
-	# 		draft.rounds.should eq 16
-	# 	end
-
-	# 	it 'should initialize with a draft type (snake or auction)' do
-	# 		draft = Draft.create(2, "snake")
-	# 		draft.type.should eq "snake"
-	# 	end
-
-	# 	it 'should initialize each team with a unique team name and number' do
-	# 		draft = Draft.create(2, "snake")
-	# 		draft.teams[1].team_name.should eq "Team 2"
-	# 	end
-
-	# 	it 'should initialize with a list of available players' do
-	# 		cam = FactoryGirl.create(:player)
-	# 		reggie = FactoryGirl.create(:reggie_bush)
-	# 		draft = Draft.create(2, "snake")
-	# 		draft.players.should eq [cam, reggie]
-	# 	end
-	# end
-
-	# describe 'change_round_number' do
-	# 	it 'should let the user change the number of rounds in the draft' do
-	# 		draft = Draft.create(2, "snake")
-	# 		draft.change_round_number(15)
-	# 		draft.rounds.should eq 15
-	# 	end
-	# end
-
-	# describe 'start' do
+	# describe 'order' do
 	# 	it 'creates the draft order if the draft type is snake' do
 	# 		draft = Draft.create(8, 'snake')
 	# 		draft.start()
